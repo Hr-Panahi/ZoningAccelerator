922229e59923aeaeaf3f18dccb1ca7e7a54c3beb
Hamid Reza Panahi <Hr.panahi@live.com>
2025-08-02 01:45:26 +0330

    Add PathHelper.cs

---
+++ Helpers/PathHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ZoningAccelerator.Helpers
+{
+    public static class PathHelper
+    {
+        public static string GetUniqueFilePath(string prefix, string cityPath, string extension)
+        {
+            var exeFolder = AppContext.BaseDirectory;
+
+            var saveDirectory = Path.Combine(exeFolder, "Saved Files");
+
+            if (!Directory.Exists(saveDirectory))
+                Directory.CreateDirectory(saveDirectory);
+
+            var cityFileName = Path.GetFileNameWithoutExtension(cityPath);
+            var safeCityName = string.Concat(cityFileName.Select(c => char.IsLetterOrDigit(c) ? c : '_'));
+
+            var baseName = $"{prefix}_{safeCityName}";
+            var fileName = $"{baseName}{extension}";
+            var fullPath = Path.Combine(saveDirectory, fileName);
+
+            int counter = 1;
+            while (File.Exists(fullPath))
+            {
+                fileName = $"{baseName}_{counter}{extension}";
+                fullPath = Path.Combine(saveDirectory, fileName);
+                counter++;
+            }
+
+            return fullPath;
+        }
+    }
+}
